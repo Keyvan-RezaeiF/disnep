eec64bdfb5a211f4f2c2d4d7b11b101ecf89a795
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2021-12-18 12:18:40 +0330

    ADD: detail

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router>
         <Header/>
-        <Home/>
+        <Routes >
+          <Route path='/' element={<Home/>} />
+          <Route path='/detail' element={<Detail/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
